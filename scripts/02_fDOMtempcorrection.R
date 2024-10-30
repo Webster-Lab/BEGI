0766c41750fb04a4d075ec9c71e963b5213fa46d
AJW <alx.webster@gmail.com>
2024-10-30 16:13:27 -0600

    small update to load in data for fDOM temp correction, but we need lab data to finish.

--- scripts/02_fDOMtempcorrection.R
+++ scripts/02_fDOMtempcorrection.R
@@ -10,3 +10,27 @@ library(zoo)
 library(stringr)
 library(suncalc)
 
+
+#### load data ####
+
+EXOz = readRDS("EXO_compiled/BEGI_EXO.or2.rds")
+
+
+#### import service date/times ####
+
+service.VDOW = read.csv("EXO_compiled/service.VDOW.csv", row.names = 1)
+names(service.VDOW) = "datetimeMT"
+service.VDOW$datetimeMT = as.POSIXct(service.VDOW$datetimeMT, tz="US/Mountain")
+
+service.VDOS = read.csv("EXO_compiled/service.VDOS.csv", row.names = 1)
+names(service.VDOS) = "datetimeMT"
+service.VDOS$datetimeMT = as.POSIXct(service.VDOS$datetimeMT, tz="US/Mountain")
+
+service.SLOC = read.csv("EXO_compiled/service.SLOC.csv", row.names = 1)
+names(service.SLOC) = "datetimeMT"
+service.SLOC$datetimeMT = as.POSIXct(service.SLOC$datetimeMT, tz="US/Mountain")
+
+service.SLOW = read.csv("EXO_compiled/service.SLOW.csv", row.names = 1)
+names(service.SLOW) = "datetimeMT"
+service.SLOW$datetimeMT = as.POSIXct(service.SLOW$datetimeMT, tz="US/Mountain")
+
