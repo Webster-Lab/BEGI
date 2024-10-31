6ca7e26008c4210542ab3e9e32cde5ebb37a8f72
“Eve <etipps830@unm.edu>
2024-10-31 12:34:29 -0600

    Updated script to save plots of 24 hr cycles. Right now, I can make the script work by running each line of code individually, but it seems to break when I run the whole for loop together.

---
+++ scripts/03_delineation.R
@@ -0,0 +1,87 @@
+#### read me ####
+
+# the purpose of this script is to delineate respiration events from the compiled EXO1 RDS files in the Webster BEGI project
+
+#### libraries ####
+library(googledrive)
+library(tidyverse)
+library(broom)
+library(zoo)
+library(stringr)
+library(suncalc)
+
+#### Import compiled EXO1 RDS file ####
+BEGI_EXO.or2 = readRDS("EXO_compiled/BEGI_EXO.or2.rds")
+
+#### Read in .csv files of service dates and times ####
+service.VDOW = read.csv("EXO_compiled/service.VDOW.csv", row.names = 1)
+names(service.VDOW) = "datetimeMT"
+service.VDOW$datetimeMT = as.POSIXct(service.VDOW$datetimeMT, tz="US/Mountain")
+
+service.VDOS = read.csv("EXO_compiled/service.VDOS.csv", row.names = 1)
+names(service.VDOS) = "datetimeMT"
+service.VDOS$datetimeMT = as.POSIXct(service.VDOS$datetimeMT, tz="US/Mountain")
+
+service.SLOC = read.csv("EXO_compiled/service.SLOC.csv", row.names = 1)
+names(service.SLOC) = "datetimeMT"
+service.SLOC$datetimeMT = as.POSIXct(service.SLOC$datetimeMT, tz="US/Mountain")
+
+service.SLOW = read.csv("EXO_compiled/service.SLOW.csv", row.names = 1)
+names(service.SLOW) = "datetimeMT"
+service.SLOW$datetimeMT = as.POSIXct(service.SLOW$datetimeMT, tz="US/Mountain")
+
+# sunrise/sunset
+
+suntimes = 
+  getSunlightTimes(date = seq.Date(from = as.Date("2023-09-14"), to = as.Date("2024-09-5"), by = 1),
+                   keep = c("sunrise", "sunset"),
+                   lat = 34.9, lon = -106.7, tz = "US/Mountain")
+
+pm.pts = suntimes$sunset[-(nrow(suntimes))]
+am.pts = suntimes$sunrise[-1]
+
+#### Vector of dates ####
+
+date <- data.frame(
+  datetimeMT = seq.POSIXt(
+    from = ISOdatetime(2023,09,15,0,0,0, tz = "US/Mountain"),
+    to = ISOdatetime(2024,09,04,0,0,0, tz= "US/Mountain"),
+    by = "24 h" ))
+
+date = as.Date(date$datetimeMT)
+
+#### plot to check ####
+
+## SLOC 24 h ##
+for (i in date) {
+
+tempdat = BEGI_EXO.or2[["SLOC"]][BEGI_EXO.or2[["SLOC"]]$datetimeMT > as.POSIXct(i, "00:00:01 MDT") &
+                                   BEGI_EXO.or2[["SLOC"]]$datetimeMT < (as.POSIXct(i, "00:00:01 MDT") +(60*60*24)),]
+
+#save plot 
+file_name = paste("plots/delineations/SLOC_", i, ".png", sep="")
+png(file_name)
+
+par(mfrow=c(2,1))
+
+plot(ymd_hms(tempdat$datetimeMT, tz="US/Mountain"),(tempdat$ODO.mg.L.mn),
+     pch=20,col="black", xlab="", xaxt = "n", type="n", ylab="")
+rect(xleft=pm.pts,xright=am.pts,ybottom=-4, ytop=100, col="lightgrey", lwd = 0)
+lines(ymd_hms(tempdat$datetimeMT, tz="US/Mountain"),(tempdat$ODO.mg.L.mn),
+      pch=20,col="black", xlab="", xaxt = "n", type="o")
+abline(v=as.POSIXct(service.SLOC$datetimeMT), col="red")
+axis.POSIXct(side=1,at=cut(tempdat$datetimeMT, breaks="24 hours"),format="%m-%d", las=2)
+title(main="Dissolved Oxygen (mg/L)")
+
+plot(ymd_hms(tempdat$datetimeMT, tz="US/Mountain"),(tempdat$fDOM.QSU.mn),
+     pch=20,col="black", xlab="", xaxt = "n", type="n", ylab="n")
+rect(xleft=pm.pts,xright=am.pts,ybottom=-4, ytop=1000, col="lightgrey", lwd = 0)
+lines(ymd_hms(tempdat$datetimeMT, tz="US/Mountain"),(tempdat$fDOM.QSU.mn),
+      pch=20,col="black", xlab="", xaxt = "n", type="o")#,ylim=c(22.5,24.5))
+abline(v=as.POSIXct(service.SLOC$datetimeMT), col="red")
+axis.POSIXct(side=1,at=cut(tempdat$datetimeMT, breaks="24 hours"),format="%m-%d", las=2)
+title(main="fDOM (QSU)")
+
+dev.off()
+}
+
