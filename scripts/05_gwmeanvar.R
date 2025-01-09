16310b2c266937316c36f538e2509268ba095b83
AJW <alx.webster@gmail.com>
2025-01-09 12:23:02 -0700

    edits and notes added to script; results for whole well exported for use in other scripts

--- scripts/05_gwmeanvar.R
+++ scripts/05_gwmeanvar.R
@@ -11,8 +11,6 @@ library(suncalc)
 library(DescTools)
 
 #### Import finalized water level data ####
-#as list
-BEGI_PTz_DTW = readRDS("DTW_compiled/BEGI_PTz_DTW.rds")
 
 #as dataframe
 BEGI_PT_DTW_all = readRDS("DTW_compiled/BEGI_PT_DTW_all.rds")
@@ -25,8 +23,8 @@ BEGI_PT_DTW_trim <- BEGI_PT_DTW_all[BEGI_PT_DTW_all$datetimeMT >= "2023-09-15 00
 #### Whole Well Mean/Var ####
 
 BEGI_PT_DTW_trim <- BEGI_PT_DTW_trim %>%
-  spread (wellID, DTW_m) %>%
-  mutate_at(c("SLOC","SLOW","VDOS","VDOW"))
+  spread (wellID, DTW_m) #%>%
+  #mutate_at(c("SLOC","SLOW","VDOS","VDOW")) # this line of code isn't working for AJW. I'm not sure what the intention is.
 
 wells<-c("SLOC","SLOW","VDOS","VDOW")
 
@@ -45,12 +43,55 @@ gwvar_well<-c(cv(BEGI_PT_DTW_trim$SLOC),
 
 gwmv_well<-data.frame(wells,gwmean_well,gwvar_well)
 gwmv_well #df of wellIDs and their average depth to water (m) and CV for the entire timeseries
+# AJW result:
+# wells gwmean_well  gwvar_well
+# 1  SLOC  -0.0102643 -971.849364
+# 2  SLOW   0.4499568   30.676837
+# 3  VDOS   1.6606177   10.563440
+# 4  VDOW   1.5886944    9.796984
+
+# check values
+mean(BEGI_PT_DTW_all$DTW_m[BEGI_PT_DTW_all$wellID=="SLOC"], na.rm = T) #-0.0155472
+cv(BEGI_PT_DTW_all$DTW_m[BEGI_PT_DTW_all$wellID=="SLOC"]) # -602.7168
+# I am not sure why these values are different from Eve's method above
+# the cv of SLOC also seems suspiciously high... how does cv handle values that move between neg and pos?
+cv(BEGI_PT_DTW_all$DTW_m[BEGI_PT_DTW_all$wellID=="SLOC" &
+                           BEGI_PT_DTW_all$DTW_m<0]) # -96.20933
+sd(BEGI_PT_DTW_all$DTW_m[BEGI_PT_DTW_all$wellID=="SLOC"], na.rm = T) # 0.09370556
+# ah... can't use cv for data with neg. values: https://stats.stackexchange.com/questions/56399/why-is-the-coefficient-of-variation-not-valid-when-using-data-with-positive-and
+# one option is to add a constant to all data so that it is all positive, though this is problematic for reasons I don't quite understand (see https://stats.stackexchange.com/questions/252714/alternative-for-coefficient-of-variation-due-to-negative-observations). I'll do it for now and address in more depth later.
+
+BEGI_PT_DTW_all$DTW_m_con = BEGI_PT_DTW_all$DTW_m + 1
+# this adds a constant to all DTW values so that none are negative
+
+# AJW version:
+gwmv_well = data.frame(wells = c("SLOC","SLOW","VDOS","VDOW"),
+                       gwmean_well = c(mean(BEGI_PT_DTW_all$DTW_m[BEGI_PT_DTW_all$wellID=="SLOC"], na.rm = T),
+                                       mean(BEGI_PT_DTW_all$DTW_m[BEGI_PT_DTW_all$wellID=="SLOW"], na.rm = T),
+                                       mean(BEGI_PT_DTW_all$DTW_m[BEGI_PT_DTW_all$wellID=="VDOS"], na.rm = T),
+                                       mean(BEGI_PT_DTW_all$DTW_m[BEGI_PT_DTW_all$wellID=="VDOW"], na.rm = T)),
+                       gwvar_well = c(cv(BEGI_PT_DTW_all$DTW_m_con[BEGI_PT_DTW_all$wellID=="SLOC"]),
+                                      cv(BEGI_PT_DTW_all$DTW_m_con[BEGI_PT_DTW_all$wellID=="SLOW"]),
+                                      cv(BEGI_PT_DTW_all$DTW_m_con[BEGI_PT_DTW_all$wellID=="VDOS"]),
+                                      cv(BEGI_PT_DTW_all$DTW_m_con[BEGI_PT_DTW_all$wellID=="VDOW"]))
+                       )
+
+#   wells gwmean_well gwvar_well
+# 1  SLOC  -0.0155472   9.518543
+# 2  SLOW   0.4561454   8.699865
+# 3  VDOS   1.6773447   5.918672
+# 4  VDOW   1.6059399   5.507157
+# these seem more reasonable and informative!
+
+# export for use in other scripts
+write.csv(gwmv_well, "DTW_compiled/gwmv_well.csv")
 
 ####import list of event dates per well ####
 BEGI_events = readRDS("EXO_compiled/BEGI_events.rds")
 
 
 #### DO event mean ####
+# note from AJW: I need the events labeled by date (or something more sophisticated if they span multiple dates) so that I can match them to the AUC etc. results for modeling!
 
 #SLOC
 #mean calculated 2 days before each event
@@ -69,7 +110,6 @@ for (i in c(1:length(BEGI_events[["DO_events"]][["SLOC_DO"]]))){
   #add mean to SLOC_event_mean
   SLOC_event_mean <- c(SLOC_event_mean,SLOC_mean)
 }
-
 
 #SLOW
 #mean calculated 2 days before each event
@@ -129,6 +169,8 @@ for (i in c(1:length(BEGI_events[["DO_events"]][["VDOW_DO"]]))){
 
 
 #### DO event CV ####
+# note from AJW: I need the events labeled by date (or something more sophisticated if they span multiple dates) so that I can match them to the AUC etc. results for modeling!
+# I also need this to be re-calculated using the DTW_m_con value of DTW (DTW + a constant so that there are no neg. values) since I discovered that CV doesn't work with data that has negatives and positives (see notes in whole well cacluation above)
 
 #SLOC
 #CV calculated 2 days before each event
