652a629e4566948d40a2fa60700030c049e3509e
“Eve <etipps830@unm.edu>
2025-01-08 19:07:43 -0700

    event level mean and cv script updated!

--- scripts/05_gwmeanvar.R
+++ scripts/05_gwmeanvar.R
@@ -53,23 +53,156 @@ BEGI_events = readRDS("EXO_compiled/BEGI_events.rds")
 #### DO event mean ####
 
 #SLOC
-#BEGI_events[["DO_events"]][["SLOC_DO"]][["SLOC_DO1"]][["datetimeMT"]][1]
-#BEGI_events[["DO_events"]][["SLOC_DO"]][[i]][["datetimeMT"]][1]
+#mean calculated 2 days before each event
 SLOC_event_mean<-numeric()
 
 for (i in c(1:length(BEGI_events[["DO_events"]][["SLOC_DO"]]))){
-  temptimes = seq(from=BEGI_events[["DO_events"]][["SLOC_DO"]][[1]]$datetimeMT[1]-(60*60*48),
-                  to=BEGI_events[["DO_events"]][["SLOC_DO"]][[1]]$datetimeMT[1], 
+  temptimes = seq(from=BEGI_events[["DO_events"]][["SLOC_DO"]][[i]]$datetimeMT[1]-(60*60*48),
+                  to=BEGI_events[["DO_events"]][["SLOC_DO"]][[i]]$datetimeMT[1], 
                   by = '15 mins')
+  SLOC_mean <- BEGI_PT_DTW_trim %>%
+    ungroup() %>%
+    filter(siteID == "SLO") %>%
+    filter(between(datetimeMT,temptimes[1],temptimes[length(temptimes)])) %>%
+    summarise(SLOC_mean = mean(SLOC, na.rm = TRUE))
   #mean of DTW_m over period of temptimes
   #add mean to SLOC_event_mean
+  SLOC_event_mean <- c(SLOC_event_mean,SLOC_mean)
 }
 
-#tempdat = BEGI_events[BEGI_events[["DO_events"]][["SLOC_DO"]][[1]]$datetimeMT >= (BEGI_events[["DO_events"]][["SLOC_DO"]][[1]]$datetimeMT[1]-(60*60*48)) &
-#  BEGI_events[["DO_events"]][["SLOC_DO"]][[1]]$datetimeMT <= (BEGI_events[["DO_events"]][["SLOC_DO"]][[1]]$datetimeMT[1]), ]
 
-#tempdat <- BEGI_events[["DO_events"]][["SLOC_DO"]][[1]]
-#tempdat_filtered <- tempdat[tempdat[["datetimeMT"]] >= (tempdat[["datetimeMT"]][1] - 60*60*48) &
-#                              tempdat[["datetimeMT"]] <= tempdat[["datetimeMT"]][1], ]
+#SLOW
+#mean calculated 2 days before each event
+SLOW_event_mean<-numeric()
 
-seq(from=BEGI_events[["DO_events"]][["SLOC_DO"]][[1]]$datetimeMT[1]-(60*60*48),to=BEGI_events[["DO_events"]][["SLOC_DO"]][[1]]$datetimeMT[1], by = '15 mins')
+for (i in c(1:length(BEGI_events[["DO_events"]][["SLOW_DO"]]))){
+  temptimes = seq(from=BEGI_events[["DO_events"]][["SLOW_DO"]][[i]]$datetimeMT[1]-(60*60*48),
+                  to=BEGI_events[["DO_events"]][["SLOW_DO"]][[i]]$datetimeMT[1], 
+                  by = '15 mins')
+  SLOW_mean <- BEGI_PT_DTW_trim %>%
+    ungroup() %>%
+    filter(siteID == "SLO") %>%
+    filter(between(datetimeMT,temptimes[1],temptimes[length(temptimes)])) %>%
+    summarise(SLOW_mean = mean(SLOW, na.rm = TRUE))
+  #mean of DTW_m over period of temptimes
+  #add mean to SLOW_event_mean
+  SLOW_event_mean <- c(SLOW_event_mean,SLOW_mean)
+}
+
+
+#VDOS
+#mean calculated 2 days before each event
+VDOS_event_mean<-numeric()
+
+for (i in c(1:length(BEGI_events[["DO_events"]][["VDOS_DO"]]))){
+  temptimes = seq(from=BEGI_events[["DO_events"]][["VDOS_DO"]][[i]]$datetimeMT[1]-(60*60*48),
+                  to=BEGI_events[["DO_events"]][["VDOS_DO"]][[i]]$datetimeMT[1], 
+                  by = '15 mins')
+  VDOS_mean <- BEGI_PT_DTW_trim %>%
+    ungroup() %>%
+    filter(siteID == "SLO") %>%
+    filter(between(datetimeMT,temptimes[1],temptimes[length(temptimes)])) %>%
+    summarise(VDOS_mean = mean(VDOS, na.rm = TRUE))
+  #mean of DTW_m over period of temptimes
+  #add mean to VDOS_event_mean
+  VDOS_event_mean <- c(VDOS_event_mean,VDOS_mean)
+}
+
+
+#VDOW
+#mean calculated 2 days before each event
+VDOW_event_mean<-numeric()
+
+for (i in c(1:length(BEGI_events[["DO_events"]][["VDOW_DO"]]))){
+  temptimes = seq(from=BEGI_events[["DO_events"]][["VDOW_DO"]][[i]]$datetimeMT[1]-(60*60*48),
+                  to=BEGI_events[["DO_events"]][["VDOW_DO"]][[i]]$datetimeMT[1], 
+                  by = '15 mins')
+  VDOW_mean <- BEGI_PT_DTW_trim %>%
+    ungroup() %>%
+    filter(siteID == "SLO") %>%
+    filter(between(datetimeMT,temptimes[1],temptimes[length(temptimes)])) %>%
+    summarise(VDOW_mean = mean(VDOW, na.rm = TRUE))
+  #mean of DTW_m over period of temptimes
+  #add mean to VDOW_event_mean
+  VDOW_event_mean <- c(VDOW_event_mean,VDOW_mean)
+}
+
+
+#### DO event CV ####
+
+#SLOC
+#CV calculated 2 days before each event
+SLOC_event_cv<-numeric()
+
+for (i in c(1:length(BEGI_events[["DO_events"]][["SLOC_DO"]]))){
+  temptimes = seq(from=BEGI_events[["DO_events"]][["SLOC_DO"]][[i]]$datetimeMT[1]-(60*60*48),
+                  to=BEGI_events[["DO_events"]][["SLOC_DO"]][[i]]$datetimeMT[1], 
+                  by = '15 mins')
+  SLOC_cv <- BEGI_PT_DTW_trim %>%
+    ungroup() %>%
+    filter(siteID == "SLO") %>%
+    filter(between(datetimeMT,temptimes[1],temptimes[length(temptimes)])) %>%
+    summarise(SLOC_cv = cv(SLOC))
+  #mean of DTW_m over period of temptimes
+  #add mean to SLOC_event_mean
+  SLOC_event_cv <- c(SLOC_event_cv,SLOC_cv)
+}
+
+
+#SLOW
+#CV calculated 2 days before each event
+SLOW_event_cv<-numeric()
+
+for (i in c(1:length(BEGI_events[["DO_events"]][["SLOW_DO"]]))){
+  temptimes = seq(from=BEGI_events[["DO_events"]][["SLOW_DO"]][[i]]$datetimeMT[1]-(60*60*48),
+                  to=BEGI_events[["DO_events"]][["SLOW_DO"]][[i]]$datetimeMT[1], 
+                  by = '15 mins')
+  SLOW_cv <- BEGI_PT_DTW_trim %>%
+    ungroup() %>%
+    filter(siteID == "SLO") %>%
+    filter(between(datetimeMT,temptimes[1],temptimes[length(temptimes)])) %>%
+    summarise(SLOW_cv = cv(SLOW))
+  #mean of DTW_m over period of temptimes
+  #add mean to SLOW_event_mean
+  SLOW_event_cv <- c(SLOW_event_cv,SLOW_cv)
+}
+
+
+#VDOS
+#CV calculated 2 days before each event
+VDOS_event_cv<-numeric()
+
+for (i in c(1:length(BEGI_events[["DO_events"]][["VDOS_DO"]]))){
+  temptimes = seq(from=BEGI_events[["DO_events"]][["VDOS_DO"]][[i]]$datetimeMT[1]-(60*60*48),
+                  to=BEGI_events[["DO_events"]][["VDOS_DO"]][[i]]$datetimeMT[1], 
+                  by = '15 mins')
+  VDOS_cv <- BEGI_PT_DTW_trim %>%
+    ungroup() %>%
+    filter(siteID == "SLO") %>%
+    filter(between(datetimeMT,temptimes[1],temptimes[length(temptimes)])) %>%
+    summarise(VDOS_cv = cv(VDOS))
+  #mean of DTW_m over period of temptimes
+  #add mean to VDOS_event_mean
+  VDOS_event_cv <- c(VDOS_event_cv,VDOS_cv)
+}
+
+
+#VDOW
+#CV calculated 2 days before each event
+VDOW_event_cv<-numeric()
+
+for (i in c(1:length(BEGI_events[["DO_events"]][["VDOW_DO"]]))){
+  temptimes = seq(from=BEGI_events[["DO_events"]][["VDOW_DO"]][[i]]$datetimeMT[1]-(60*60*48),
+                  to=BEGI_events[["DO_events"]][["VDOW_DO"]][[i]]$datetimeMT[1], 
+                  by = '15 mins')
+  VDOW_cv <- BEGI_PT_DTW_trim %>%
+    ungroup() %>%
+    filter(siteID == "SLO") %>%
+    filter(between(datetimeMT,temptimes[1],temptimes[length(temptimes)])) %>%
+    summarise(VDOW_cv = cv(VDOW))
+  #mean of DTW_m over period of temptimes
+  #add mean to VDOW_event_mean
+  VDOW_event_cv <- c(VDOW_event_cv,VDOW_cv)
+}
+
+
