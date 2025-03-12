cef439ed17051a6016947cd2d76d26374111b6a5
“Eve <etipps830@unm.edu>
2025-03-12 12:12:32 -0600

    fixed NA values in AUC; calculated and plotted positive ER AUC

--- scripts/09_DOtoER.R
+++ scripts/09_DOtoER.R
@@ -98,12 +98,32 @@ for (name in names(roc_all[["VDOS_rates"]])){
 }
 
 #### Plot to check ####
-#plot ER v time to check
-#plot(roc_all[["SLOC_rates"]][["SLOC_DO2"]]$datetimeMT,roc_all[["SLOC_rates"]][["SLOC_DO2"]]$ER,
-#     xlab = "date time",
-#     ylab = "ER (g O2/m^2/15 min)",
-#     type="l")
-#abline(h=0,col="blue")
+#plot ER v time to check (SLOC_DO4: 10/16)
+plot(roc_all[["SLOC_rates"]][["SLOC_DO4"]]$datetimeMT,roc_all[["SLOC_rates"]][["SLOC_DO4"]]$ER,
+     xlab = "date time",
+     ylab = "ER (g O2/m^2/15 min)",
+     type="l")
+abline(h=0,col="blue")
+#plot DO rate v time to compare
+plot(roc_all[["SLOC_rates"]][["SLOC_DO4"]]$datetimeMT,roc_all[["SLOC_rates"]][["SLOC_DO4"]]$rate_of_change_g,
+     xlab = "date time",
+     ylab = "DO (g O2/m^2/15 min)",
+     type="l")
+abline(h=0,col="blue")
+
+#plot ER v time to check (SLOC_DO5: 10/17-18)
+plot(roc_all[["SLOC_rates"]][["SLOC_DO5"]]$datetimeMT,roc_all[["SLOC_rates"]][["SLOC_DO5"]]$ER,
+     xlab = "date time",
+     ylab = "ER (g O2/m^2/15 min)",
+     type="l")
+abline(h=0,col="blue")
+#plot DO rate v time to compare
+plot(roc_all[["SLOC_rates"]][["SLOC_DO5"]]$datetimeMT,roc_all[["SLOC_rates"]][["SLOC_DO5"]]$rate_of_change_g,
+     xlab = "date time",
+     ylab = "DO (g O2/m^2/15 min)",
+     type="l")
+abline(h=0,col="blue")
+
 
 #### Integrate under ER curve ####
 #AUC(x = (as.numeric(roc_all[["SLOC_rates"]][["SLOC_DO1"]]$datetimeMT)[-1]),
@@ -118,16 +138,23 @@ SLOC_ER_results <- numeric(length(roc_all[["SLOC_rates"]]))
 names(SLOC_ER_results) <- names(roc_all[["SLOC_rates"]])
 
 for (i in seq_along(roc_all[["SLOC_rates"]])) {
+  x_vals <- as.numeric(roc_all[["SLOC_rates"]][[i]]$datetimeMT)
+  y_vals <- roc_all[["SLOC_rates"]][[i]]$ER
+  
+# Remove NA values
+  valid_indices <- !is.na(y_vals)
+  x_vals <- x_vals[valid_indices]
+  y_vals <- y_vals[valid_indices]
+
   SLOC_ER_results[i] <- AUC(
-    x = as.numeric(roc_all[["SLOC_rates"]][[i]]$datetimeMT)[-1],
-    y = roc_all[["SLOC_rates"]][[i]]$ER[-1],
+    x = x_vals,
+    y = y_vals,
     method = "trapezoid",
     na.rm = FALSE
   )
 }
 
 View(SLOC_ER_results)
-#shouldn't be getting negative values with AUC? 
 
 
 #SLOW#
@@ -136,16 +163,25 @@ SLOW_ER_results <- numeric(length(roc_all[["SLOW_rates"]]))
 names(SLOW_ER_results) <- names(roc_all[["SLOW_rates"]])
 
 for (i in seq_along(roc_all[["SLOW_rates"]])) {
+  x_vals <- as.numeric(roc_all[["SLOW_rates"]][[i]]$datetimeMT)
+  y_vals <- roc_all[["SLOW_rates"]][[i]]$ER
+  
+  # Remove NA values
+  valid_indices <- !is.na(y_vals)
+  x_vals <- x_vals[valid_indices]
+  y_vals <- y_vals[valid_indices]
+  
   SLOW_ER_results[i] <- AUC(
-    x = as.numeric(roc_all[["SLOW_rates"]][[i]]$datetimeMT)[-1],
-    y = roc_all[["SLOW_rates"]][[i]]$ER[-1],
+    x = x_vals,
+    y = y_vals,
     method = "trapezoid",
     na.rm = FALSE
   )
 }
 
 View(SLOW_ER_results)
-#Need to fix code to account for NA values that are not in the first position of each dataframe
+
+
 
 #VDOW#
 
@@ -153,9 +189,17 @@ VDOW_ER_results <- numeric(length(roc_all[["VDOW_rates"]]))
 names(VDOW_ER_results) <- names(roc_all[["VDOW_rates"]])
 
 for (i in seq_along(roc_all[["VDOW_rates"]])) {
+  x_vals <- as.numeric(roc_all[["VDOW_rates"]][[i]]$datetimeMT)
+  y_vals <- roc_all[["VDOW_rates"]][[i]]$ER
+  
+  # Remove NA values
+  valid_indices <- !is.na(y_vals)
+  x_vals <- x_vals[valid_indices]
+  y_vals <- y_vals[valid_indices]
+  
   VDOW_ER_results[i] <- AUC(
-    x = as.numeric(roc_all[["VDOW_rates"]][[i]]$datetimeMT)[-1],
-    y = roc_all[["VDOW_rates"]][[i]]$ER[-1],
+    x = x_vals,
+    y = y_vals,
     method = "trapezoid",
     na.rm = FALSE
   )
@@ -164,15 +208,24 @@ for (i in seq_along(roc_all[["VDOW_rates"]])) {
 View(VDOW_ER_results)
 
 
+
 #VDOS#
 
 VDOS_ER_results <- numeric(length(roc_all[["VDOS_rates"]])) 
 names(VDOS_ER_results) <- names(roc_all[["VDOS_rates"]])
 
 for (i in seq_along(roc_all[["VDOS_rates"]])) {
+  x_vals <- as.numeric(roc_all[["VDOS_rates"]][[i]]$datetimeMT)
+  y_vals <- roc_all[["VDOS_rates"]][[i]]$ER
+  
+  # Remove NA values
+  valid_indices <- !is.na(y_vals)
+  x_vals <- x_vals[valid_indices]
+  y_vals <- y_vals[valid_indices]
+  
   VDOS_ER_results[i] <- AUC(
-    x = as.numeric(roc_all[["VDOS_rates"]][[i]]$datetimeMT)[-1],
-    y = roc_all[["VDOS_rates"]][[i]]$ER[-1],
+    x = x_vals,
+    y = y_vals,
     method = "trapezoid",
     na.rm = FALSE
   )
@@ -198,5 +251,132 @@ ER_AUC_bp<-ggplot(data=ER_AUC,mapping=aes(x=Well, y=ER))+geom_boxplot(fill=c("#4
 print(ER_AUC_bp)
 
 
+#### Positive ER integral ####
 
+#SLOC#
+SLOC_posER_results <- numeric(length(roc_all[["SLOC_rates"]])) 
+names(SLOC_posER_results) <- names(roc_all[["SLOC_rates"]])
+
+for (i in seq_along(roc_all[["SLOC_rates"]])) {
+  x_vals <- as.numeric(roc_all[["SLOC_rates"]][[i]]$datetimeMT)
+  y_vals <- roc_all[["SLOC_rates"]][[i]]$ER
+  
+  # Remove NA values
+  valid_indices <- !is.na(y_vals)
+  x_vals <- x_vals[valid_indices]
+  y_vals <- y_vals[valid_indices]
+  
+  # Set negative values to zero to exclude them from integration
+  y_vals[y_vals < 0] <- 0
+
+
+  SLOC_posER_results[i] <- AUC(
+    x = x_vals,
+    y = y_vals,
+    method = "trapezoid",
+    na.rm = FALSE
+  )
+}
+
+View(SLOC_posER_results)
+
+
+#SLOW#
+SLOW_posER_results <- numeric(length(roc_all[["SLOW_rates"]])) 
+names(SLOW_posER_results) <- names(roc_all[["SLOW_rates"]])
+
+for (i in seq_along(roc_all[["SLOW_rates"]])) {
+  x_vals <- as.numeric(roc_all[["SLOW_rates"]][[i]]$datetimeMT)
+  y_vals <- roc_all[["SLOW_rates"]][[i]]$ER
+  
+  # Remove NA values
+  valid_indices <- !is.na(y_vals)
+  x_vals <- x_vals[valid_indices]
+  y_vals <- y_vals[valid_indices]
+  
+  # Set negative values to zero to exclude them from integration
+  y_vals[y_vals < 0] <- 0
+  
+  
+  SLOW_posER_results[i] <- AUC(
+    x = x_vals,
+    y = y_vals,
+    method = "trapezoid",
+    na.rm = FALSE
+  )
+}
+
+View(SLOW_posER_results)
+
+
+#VDOW#
+VDOW_posER_results <- numeric(length(roc_all[["VDOW_rates"]])) 
+names(VDOW_posER_results) <- names(roc_all[["VDOW_rates"]])
+
+for (i in seq_along(roc_all[["VDOW_rates"]])) {
+  x_vals <- as.numeric(roc_all[["VDOW_rates"]][[i]]$datetimeMT)
+  y_vals <- roc_all[["VDOW_rates"]][[i]]$ER
+  
+  # Remove NA values
+  valid_indices <- !is.na(y_vals)
+  x_vals <- x_vals[valid_indices]
+  y_vals <- y_vals[valid_indices]
+  
+  # Set negative values to zero to exclude them from integration
+  y_vals[y_vals < 0] <- 0
+  
+  
+  VDOW_posER_results[i] <- AUC(
+    x = x_vals,
+    y = y_vals,
+    method = "trapezoid",
+    na.rm = FALSE
+  )
+}
+
+View(VDOW_posER_results)
+
+
+#VDOS#
+VDOS_posER_results <- numeric(length(roc_all[["VDOS_rates"]])) 
+names(VDOS_posER_results) <- names(roc_all[["VDOS_rates"]])
+
+for (i in seq_along(roc_all[["VDOS_rates"]])) {
+  x_vals <- as.numeric(roc_all[["VDOS_rates"]][[i]]$datetimeMT)
+  y_vals <- roc_all[["VDOS_rates"]][[i]]$ER
+  
+  # Remove NA values
+  valid_indices <- !is.na(y_vals)
+  x_vals <- x_vals[valid_indices]
+  y_vals <- y_vals[valid_indices]
+  
+  # Set negative values to zero to exclude them from integration
+  y_vals[y_vals < 0] <- 0
+  
+  
+  VDOS_posER_results[i] <- AUC(
+    x = x_vals,
+    y = y_vals,
+    method = "trapezoid",
+    na.rm = FALSE
+  )
+}
+
+View(VDOS_posER_results)
+
+#### plot positive ER boxplot ####
+
+posER_AUC<-data.frame(posER=c(SLOC_posER_results,SLOW_posER_results,VDOW_posER_results,VDOS_posER_results),
+                   Well=rep(c("SLOC","SLOW","VDOW","VDOS"),
+                            times=c(length(SLOC_posER_results),length(SLOW_posER_results),length(VDOW_posER_results),length(VDOS_posER_results))))
+posER_AUC$Event<-c('SLOC_DO1','SLOC_DO2','SLOC_DO3','SLOC_DO4','SLOC_DO5','SLOC_DO6','SLOC_DO7','SLOC_DO8','SLOC_DO9','SLOC_DO10',
+                'SLOC_DO11','SLOC_DO12','SLOC_DO13','SLOC_DO14','SLOC_DO15','SLOC_DO16','SLOC_DO17','SLOC_DO18','SLOC_DO19','SLOC_DO20',
+                'SLOC_DO21','SLOW_DO1','SLOW_DO2','SLOW_DO3','SLOW_DO4','SLOW_DO5','SLOW_DO6','SLOW_DO7','SLOW_DO8','SLOW_DO9','SLOW_DO10',
+                'VDOW_DO1','VDOW_DO2','VDOW_DO3','VDOW_DO4','VDOW_DO5','VDOW_DO6','VDOW_DO7','VDOW_DO8','VDOW_DO9','VDOW_DO10',
+                'VDOW_DO11','VDOW_DO12','VDOW_DO13','VDOW_DO14','VDOW_DO15','VDOW_DO16','VDOW_DO17','VDOW_DO18',
+                'VDOS_DO1','VDOS_DO2','VDOS_DO3','VDOS_DO4','VDOS_DO5','VDOS_DO6','VDOS_DO7','VDOS_DO8','VDOS_DO9','VDOS_DO10')
+#ER_AUC$Eventdate <-c(SLOC_dates,SLOW_dates,VDOW_dates,VDOS_dates) 
+
+ER_AUC_bp<-ggplot(data=posER_AUC,mapping=aes(x=Well, y=posER))+geom_boxplot(fill=c("#440154FF","#31688EFF","#35B779FF","#FDE725FF"))+labs(y = "Net Positive Ecosystem Respiration (g O2/m^2)")
+print(ER_AUC_bp)
 
